c39baa29080fc3214126eba519b582ec757c4a45
Lorian <lanthala@gmail.com>
2014-07-20 20:01:48 -0700

    Updated paths to point to new ~/tools/ repository

--- M_full_allgenome_pipeline.sh
+++ M_full_allgenome_pipeline.sh
@@ -1,7 +1,7 @@
 # Version 1.1 -- used for testM
 
 #sed -e '/>/s/^/@/' -e '/>/s/$/#/' Martin_etal_TextS3_13Dec2011_poundless.fasta | tr -d "\n" | tr "@" "\n" | sort -t "|" -k1n | tr "#" "\n" | sed -e '/^$/d' > Martin_etal_TextS3_13Dec2011_sorted.fasta \
-#&& python M_process_metagenome_reference.py Martin_etal_TextS3_13Dec2011_sorted.fasta \
+#&& python ~/tools/M_process_metagenome_reference.py Martin_etal_TextS3_13Dec2011_sorted.fasta \
 #&&bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_0.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_0 \
 #&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_1.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_1 \
 #&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_2.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_2 \
@@ -12,7 +12,7 @@
 #&& bowtie2 -a -t -p 30 --local -x Martin_etal_TextS3_13Dec2011_sorted_genomes_2 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testM_g2.SAM \
 #&& bowtie2 -a -t -p 30 --local -x Martin_etal_TextS3_13Dec2011_sorted_plasmids_0 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testM_p.SAM \
 
-python M_merge_sam_files.py testM_g0.SAM testM_g1.SAM testM_g2.SAM testM_p.SAM testE.SAM \
+python ~/tools/M_merge_sam_files.py testM_g0.SAM testM_g1.SAM testM_g2.SAM testM_p.SAM testE.SAM \
 && mv combined_file.sam testE_M.sam \
 && samtools view -bhS testE_M.sam > testE_M.bam \
 && ulimit -n 5000 \
