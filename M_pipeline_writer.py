52e0c7abd87ca5799c21077c170bd84696e5c240
Lorian <lanthala@gmail.com>
2014-07-21 15:14:12 -0700

    Script that writes a full pipeline shell script, given parameters and existing files. Currently set up to produce version 1.2, for testL_J.

---
+++ M_pipeline_writer.py
@@ -0,0 +1,120 @@
+"""
+Takes file names and parameters and what's known about existing files and writes a full pipeline shell script
+
+Format of script:
+# Version 1.2 -- used for testL
+cat Martin_etal_TextS3_13Dec2011.fasta | tr -d \# > Martin_etal_TextS3_13Dec2011_poundless.fasta
+&& sed -e '/>/s/^/@/' -e '/>/s/$/#/' Martin_etal_TextS3_13Dec2011_poundless.fasta | tr -d "\n" | tr "@" "\n" | sort -t "|" -k1n | tr "#" "\n" | sed -e '/^$/d' > Martin_etal_TextS3_13Dec2011_poundless.fasta \
+&& python ~/tools/M_process_metagenome_reference.py Martin_etal_TextS3_13Dec2011_sorted.fasta \
+&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_0.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_0 \
+&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_1.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_1 \
+&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_genomes_2.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_2 \
+&& bowtie2-build Martin_etal_TextS3_13Dec2011_sorted_plasmids_0.mfa Martin_etal_TextS3_13Dec2011_sorted_plasmids_0 \
+&& bowtie2 -a -t -p 40 --local -x Martin_etal_TextS3_13Dec2011_sorted_genomes_0 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testL_g0.SAM \
+&& bowtie2 -a -t -p 40 --local -x Martin_etal_TextS3_13Dec2011_sorted_genomes_1 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testL_g1.SAM \
+&& bowtie2 -a -t -p 40 --local -x Martin_etal_TextS3_13Dec2011_sorted_genomes_2 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testL_g2.SAM \
+&& bowtie2 -a -t -p 40 --local -x Martin_etal_TextS3_13Dec2011_sorted_plasmids_0 -1 illumina_100species.1.fq -2 illumina_100species.2.fq -S testL_p.SAM \
+&& python ~/tools/M_merge_sam_files.py testL_g0.SAM testL_g1.SAM testL_g2.SAM testL_p.SAM testJ.SAM \
+&& mv combined_file.sam testL_J.SAM \
+&& samtools view -bhS testL_J.SAM > testL_J.bam \
+&& ulimit -n 5000 \
+&& samtools sort -n testL_J.bam testL_J_sorted \
+&& cat illumina_100genomes.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_0.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_1.mfa Martin_etal_TextS3_13Dec2011_sorted_genomes_2.mfa Martin_etal_TextS3_13Dec2011_sorted_plasmids_0.mfa > Martin_etal_TextS3_13Dec2011_sorted_i100.fasta \
+&& express -f 0.85 --max-indel-size 100 -B 20 Martin_etal_TextS3_13Dec2011_sorted_i100.fasta testL_J_sorted.bam
+"""
+
+import os
+
+use_existing_files = True
+
+def missing_file(filename):
+	if use_existing_files and os.path.exists(filename):
+		return False
+	else:
+		return True # consider adding lines to the shell script to delete files that will be regenerated
+
+def insert_suffix(filename,suffix,extension=""):
+	if extension:
+		return filename.rpartition('.')[0] + suffix +"."+ extension
+	else:
+		return filename.rpartition('.')[0] + suffix +"."+ filename.rpartition('.')[2]
+
+def main():
+	# Filename constants
+	test_basename = 'testL'
+	version = '1.2'
+	cores = 40
+	raw_fasta_file = 'Martin_etal_TextS3_13Dec2011.fasta'
+	fastq_file_r1 = "illumina_100species_trimmed.1.fq.gz"
+	fastq_file_r2 = "illumina_100species_trimmed.2.fq.gz"
+	i100_alignment = 'testJ.SAM'
+	i100_fasta = 'illumina_100genomes.mfa'
+	express_cycles = 20
+	express_f = 0.85
+
+	script = open("M_pipeline_{0}.sh".format(test_basename),'w')
+	script.write("# Version {0} -- used for {1}\n".format(version,test_basename))
+
+	# Removes pound sign from fasta file
+	cleaned_fasta_file = insert_suffix(raw_fasta_file, '_poundless')
+	if missing_file(cleaned_fasta_file):
+		script.write('cat {0} | tr -d \\# > {1}\n&& '.format(raw_fasta_file,cleaned_fasta_file))
+
+	# Sorts fasta by name of genome
+	sorted_fasta_file = insert_suffix(raw_fasta_file, '_sorted')
+	if missing_file(sorted_fasta_file):
+		script.write('sed -e \'/>/s/^/@/\' -e \'/>/s/$/#/\' {0} | tr -d "\\n" | tr "@" "\\n" | sort -t "|" -k1n | tr "#" "\\n" | sed -e \'/^$/d\' > {0} \\\n&& '
+		             .format(cleaned_fasta_file,sorted_fasta_file))
+
+	# Breaks fasta into bowtie-sized chunks for indexing
+	all_fastas = [insert_suffix(sorted_fasta_file,'_genomes_0','mfa'),
+	              insert_suffix(sorted_fasta_file,'_genomes_1','mfa'),
+	              insert_suffix(sorted_fasta_file,'_genomes_2','mfa'),
+	              insert_suffix(sorted_fasta_file,'_plasmids_0','mfa')]
+	if any(( [missing_file(f) for f in all_fastas] )):
+		script.write('python ~/tools/M_process_metagenome_reference.py {0} \\\n&& '.format(sorted_fasta_file))
+
+	# Builds bowtie2 indexes
+	for fasta in all_fastas:
+		if any(( missing_file(insert_suffix(fasta,"","1.bt2")), missing_file(insert_suffix(fasta,"","2.bt2")),
+		         missing_file(insert_suffix(fasta,"","3.bt2")), missing_file(insert_suffix(fasta,"","4.bt2")),
+		         missing_file(insert_suffix(fasta,"","rev.1.bt2")), missing_file(insert_suffix(fasta,"","rev.2.bt2")) )):
+			script.write('bowtie2-build {0} {1} \\\n&& '.format(fasta,fasta.rpartition(".")[0]))
+
+	# Run bowtie2
+	all_suffixes = ['_g0', '_g1', '_g2', '_p'] # unable to figure out a way to automatically generate these
+	for i,fasta in enumerate(all_fastas):
+		if missing_file(test_basename + all_suffixes[i]):
+			script.write('bowtie2 -a -t -p {0} --local -x {1} -1 {2} -2 {3} -S {4}.SAM \\\n&& '
+		                 .format(cores,fasta.rpartition(".")[0],fastq_file_r1,fastq_file_r2,test_basename + all_suffixes[i]))
+
+	# Merge sam files
+	merged_sam_file = "{0}_{1}".format(test_basename,i100_alignment.rpartition("test")[2])
+	if missing_file(merged_sam_file):
+		script.write('python ~/tools/M_merge_sam_files.py {0} {1} \\\n&& '
+		             .format(" ".join([test_basename + s + '.SAM' for s in all_suffixes]),i100_alignment))
+		script.write('mv combined_file.sam {0} \\\n&& '.format(merged_sam_file))
+
+	# Convert to bam
+	bam_file = insert_suffix(merged_sam_file,"","bam")
+	if missing_file(bam_file):
+		script.write('samtools view -bhS {0} > {1} \\\n&& '.format(merged_sam_file,bam_file))
+
+	# Sort
+	sorted_bam_file = insert_suffix(bam_file,"_sorted")
+	if missing_file(sorted_bam_file):
+		script.write('ulimit -n 5000 \\\n&& ')
+		script.write('samtools sort -n {0} {1} \\\n&& '.format(bam_file,sorted_bam_file.rpartition(".")[0]))
+
+	# Combine all mfa files used
+	merged_all_fastas = insert_suffix(sorted_fasta_file,"_i100")
+	if missing_file(merged_all_fastas):
+		script.write('cat {0} {1} > {2} \\\n&& '
+		             .format(i100_fasta," ".join([f for f in all_fastas]),merged_all_fastas))
+
+	# Run express (assuming this will always be run, otherwise why run the script)
+	script.write('express -f {0} --max-indel-size 100 -B {1} {2} {3}'
+	             .format(express_f,express_cycles,merged_all_fastas,sorted_bam_file))
+
+if __name__ == '__main__':
+	main()
