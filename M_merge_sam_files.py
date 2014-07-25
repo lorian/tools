c36987385243df96d2ae273e9a9fe4a680e4baf7
Lorian <lanthala@gmail.com>
2014-07-24 23:05:36 -0700

    Removed conditional check for previously-made _justbody.txt files
    If the script was interrupted while making those files, they ended up truncated without warning, causing the resulting merge to be missing parts. Also added print statements to explain what's taking so long.

--- M_merge_sam_files.py
+++ M_merge_sam_files.py
@@ -11,10 +11,10 @@ next(iterarg) #skip name of function
 filenames = []
 for arg in iterarg:
 	filenames.append(arg)
+print filenames
 
 samfiles = []
 for name in filenames:
-	print name
 	samfiles.append(pysam.Samfile(name,'r'))
 
 full_references = []
@@ -23,13 +23,15 @@ for f in samfiles:
 	full_references = full_references + [r for r in f.references]
 	full_lengths = full_lengths + [r for r in f.lengths]
 
+print "Creating combined header..."
 header_sam = pysam.Samfile('combined_header.sam', mode='wh', referencenames=full_references, referencelengths=full_lengths)
 header_sam.close()
 
 full_sam = open('combined_file.sam','w')
 shutil.copyfileobj(open('combined_header.sam','r'), full_sam)
 for name in filenames:
-	if not os.path.isfile(name + '_justbody.txt'): #don't recreate body file if it already exists from previous failed runs
-		os.system('samtools view -S -o {0}_justbody.txt {0}'.format(name))
+	print "Copying {0}".format(name)
+	#if not os.path.isfile(name + '_justbody.txt'): #don't recreate body file if it already exists from previous failed runs
+	os.system('samtools view -S -o {0}_justbody.txt {0}'.format(name))
 	shutil.copyfileobj(open(name + '_justbody.txt','r'), full_sam)
 full_sam.close()
