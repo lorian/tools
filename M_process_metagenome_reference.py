eff44d14dc0038da392e2fbf3c0f64e393437d5a
Lorian <lanthala@gmail.com>
2014-09-15 23:17:40 -0700

    Remove duplicate i100 genomes from Martin fasta

--- M_process_metagenome_reference.py
+++ M_process_metagenome_reference.py
@@ -10,8 +10,11 @@
 
 import sys
 import os
+import math
+import lanthpy
 
 split_size = 3500000000 # 3.5gb
+fragment_size = 500000 # bp
 
 #Note: File needs to be sorted!
 filename = ""
@@ -30,8 +33,8 @@ file_count_p = 0
 line_count = 0
 
 # Open files
-genomes_string = filename.rsplit('.', 1)[0] + "_genomes_{0}.mfa" #mfa file format is for compatibility with my mass-indexing script
-plasmids_string = filename.rsplit('.', 1)[0] + "_plasmids_{0}.mfa"
+genomes_string = filename.rsplit('.', 1)[0] + "_fragmented_genomes_{0}.mfa" #mfa file format is for compatibility with mass-indexing script
+plasmids_string = filename.rsplit('.', 1)[0] + "_fragmented_plasmids_{0}.mfa"
 
 fa_genomes = open(genomes_string.format(file_count_g), 'wt')
 fa_genomes.seek(0) #overwrite if it exists
@@ -40,36 +43,61 @@ fa_plasmids.seek(0) #overwrite if it exists
 
 last_species = "."
 last_type = ""
+last_genome = ""
+skip_current = False
+
+# Remove these strains from Martin file
+i100_species = ['acinetobacter_baumannii_sdf', 'alkalilimnicola_ehrlichii_mlhe1', 'alkaliphilus_metalliredigens_qymf', 'anabaena_variabilis_atcc_29413', 'bacillus_anthracis_str_ames', 'bacillus_cereus_atcc_10987', 'bacillus_cereus_atcc_14579', 'bacillus_cereus_e33l', 'bacillus_clausii_ksmk16', 'bacillus_halodurans_c125', 'bacillus_subtilis_subsp_subtilis_str_168', 'bacillus_thuringiensis_str_al_hakam', 'bacteroides_thetaiotaomicron_vpi5482', 'bordetella_bronchiseptica_rb50', 'borrelia_garinii_pbi', 'buchnera_aphidicola_bcc', 'burkholderia_phymatum_stm815', 'burkholderia_pseudomallei_668', 'campylobacter_concisus_13826', 'candidatus_blochmannia_pennsylvanicus_str_bpen', 'chlamydia_trachomatis_l2buch1proctitis', 'chlamydophila_caviae_gpic', 'chlamydophila_pneumoniae_cwl029', 'chlorobium_luteolum_dsm_273', 'chromobacterium_violaceum_atcc_12472', 'clostridium_beijerinckii_ncimb_8052', 'clostridium_botulinum_b1_str_okra', 'clostridium_novyi_nt', 'clostridium_thermocellum_atcc_27405', 'corynebacterium_efficiens_ys314', 'corynebacterium_urealyticum_dsm_7109', 'coxiella_burnetii_rsa_493', 'cyanothece_sp_atcc_51142', 'cytophaga_hutchinsonii_atcc_33406', 'erwinia_tasmaniensis', 'escherichia_coli_apec_o1', 'escherichia_coli_e24377a', 'escherichia_coli_str_k12_substr_mg1655', 'escherichia_coli_str_k12_substr_w3110', 'francisella_tularensis_subsp_tularensis_fsc198', 'geobacter_sulfurreducens_pca', 'haemophilus_influenzae_rd_kw20', 'haemophilus_somnus_129pt', 'haloquadratum_walsbyi_dsm_16790', 'herpetosiphon_aurantiacus_dsm_785', 'hyphomonas_neptunium_atcc_15444', 'idiomarina_loihiensis_l2tr', 'ignicoccus_hospitalis_kin4i', 'lactobacillus_delbrueckii_subsp_bulgaricus_atcc_baa365', 'lactobacillus_salivarius_ucc118', 'lactococcus_lactis_subsp_cremoris_sk11', 'lactococcus_lactis_subsp_lactis_il1403', 'lawsonia_intracellularis_phemn100', 'leptospira_borgpetersenii_serovar_hardjobovis_jb197', 'listeria_welshimeri_serovar_6b_str_slcc5334', 'methanococcus_maripaludis_c7', 'mycobacterium_avium_subsp_paratuberculosis_k10', 'mycobacterium_bovis_af212297', 'mycobacterium_marinum_m', 'mycobacterium_sp_jls', 'mycobacterium_tuberculosis_f11', 'neisseria_gonorrhoeae_fa_1090', 'neisseria_meningitidis_mc58', 'nostoc_punctiforme_pcc_73102', 'ochrobactrum_anthropi_atcc_49188', 'prochlorococcus_marinus_str_mit_9313', 'prochlorococcus_marinus_str_natl1a', 'pseudomonas_putida_w619', 'pseudomonas_stutzeri_a1501', 'psychrobacter_cryohalolentis_k5', 'psychromonas_ingrahamii_37', 'ralstonia_eutropha_h16', 'rhodococcus_jostii_rha1', 'rhodopseudomonas_palustris_bisa53', 'rhodopseudomonas_palustris_cga009', 'rickettsia_prowazekii_str_madrid_e', 'salmonella_enterica_subsp_enterica_serovar_paratyphi_b_str_spb7', 'shewanella_sp_mr7', 'sinorhizobium_meliloti_1021', "sodalis_glossinidius_str_morsitans", 'staphylococcus_aureus_subsp_aureus_jh1', 'staphylococcus_aureus_subsp_aureus_mrsa252', 'staphylococcus_aureus_subsp_aureus_mu3', 'staphylococcus_aureus_subsp_aureus_mw2', 'staphylococcus_aureus_subsp_aureus_nctc_8325', 'staphylococcus_aureus_subsp_aureus_str_newman', 'streptococcus_agalactiae_2603vr', 'streptococcus_pneumoniae_r6', 'streptococcus_pyogenes_mgas315', 'streptococcus_pyogenes_mgas5005', 'synechococcus_elongatus_pcc_6301', 'synechococcus_elongatus_pcc_7942', 'syntrophomonas_wolfei_subsp_wolfei_str_goettingen', 'thermosipho_melanesiensis_bi429', 'thermus_thermophilus_hb8', 'thiobacillus_denitrificans_atcc_25259', 'treponema_denticola_atcc_35405', 'ureaplasma_parvum_serovar_3_str_atcc_700970', 'wigglesworthia_glossinidia_endosymbiont_of_glossina_brevipalpis', 'yersinia_pestis_co92']
+
+if filename.startswith('Martin'):
+	duplicates = i100_species
+else:
+	duplicates = []
 
 for line in mfa:
 	if line[0] == '>': # fasta name
+		skip_current = False
+
+		if last_genome: # if we have a genome in buffer, we need to chop it and write it before handling the new entry
+
+			# Calculate most balanced length of genome fragments, to avoid having particularly short fragments
+			num_of_fragments = len(last_genome)/fragment_size # auto-rounded to nearest int
+			length_of_fragments = int(math.ceil(len(last_genome)/float(max(num_of_fragments,1)))) # rounded up to nearest bp
+#			print "Species: {0}\tGenome length: {1}\t# Fragments: {2}\tFragment length: {3}".format(last_species,len(last_genome),num_of_fragments,length_of_fragments)
+
+			for i in range(0,len(last_genome),length_of_fragments):
+				fa_genomes.write(last_species[:-1] + str(i) +'\n') #insert char number in fasta name
+				fa_genomes.write(last_genome[i:i+length_of_fragments]+'\n')
+
+			last_genome = "" # wipe last genome once it's written
+
 		line = line.replace (" ", "_")
 		if line.find('VIRL') != -1: #ignore viruses
 			last_type = 'virus'
 		elif line.find('plasmid') != -1: #just copy plasmids
 			last_type = 'plasmid'
 			fa_plasmids.write(line)
-		else: #attempt to "assemble" genomes
-			last_type = 'genome'
-			species,x,y = line.partition('|')
-			if last_species == species:
-				print "Species duplicate of {0} in {1}".format(species,line)
-				fa_genomes.write('NNNNNNNNNN') # potential gap
+		else: # prepare to chop genomes up into pieces
+				# skip duplicates
+		if lanthpy.genome_name_cleanup([line]):
+			if any(species in line for species in duplicates):
+				skip_current = True
 			else:
-				print "Species NOT duplicate of {0} in {1}".format(last_species,line)
+				last_type = 'genome'
+				last_species = line # for purposes of labeling the genome fragments
+
 				# Split file past size limit, but only between species entries
 				fa_genomes.flush()
-				filesize = os.fstat( fa_genomes.fileno() )
+				filesize = os.fstat(fa_genomes.fileno())
 				if filesize.st_size > split_size:
 					fa_genomes.close()
 					file_count_g += 1
 					fa_genomes = open(genomes_string.format(file_count_g), 'wt')
 
-				fa_genomes.write(line)
-				last_species = species
 	else: #actual fasta content
-		if last_type == 'genome':
-			fa_genomes.write(line)
+		if last_type == 'genome' && not skip_current: # first store entire genome in variable
+			last_genome += line
+
 		elif last_type == 'plasmid':
 			fa_plasmids.write(line)
 
