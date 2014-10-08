bd909b45eb958359a0ad22a132f87454a43b7642
Lorian <lanthala@gmail.com>
2014-10-08 13:29:25 -0700

    Compares fasta lengths with sam lengths

--- M_fasta_sequence_length.py
+++ M_fasta_sequence_length.py
@@ -2,12 +2,24 @@
 from Bio import SeqIO
 import sys
 import pprint
+import csv
 
 # Modified from http://bioexpressblog.wordpress.com/2014/04/15/calculate-length-of-all-sequences-in-an-multi-fasta-file/
 
+# Get fasta lengths
 fasta_sizes = dict()
 cmdargs = str(sys.argv)
 for seq_record in SeqIO.parse(str(sys.argv[1]), "fasta"):
 	fasta_sizes[seq_record.id] = len(seq_record)
 
-print fasta_sizes['EUKY_12|gi|134108779|ref|NC_009178.1|_Cryptococcus_neoformans_var._neoformans_B-3501A_chromosome_2,_whole_genome_shotgun_sequence913558']
+# Format of sam header: @SQ, SN:<name>, LN:<length>
+sam_header = csv.reader(open('testE_head.sam','r'),delimiter='\t')
+for row in sam_header:
+	if row[1][3:] not in fasta_sizes.keys():
+		print row
+	else:
+		if int(row[2][3:]) != fasta_sizes[row[1][3:]]:
+
+			print row
+			print fasta_sizes[row[1][3:]]
+
