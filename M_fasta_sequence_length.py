727a0159a0171747baf0f92327554f9880b91eaa
Lorian <lanthala@gmail.com>
2014-10-07 22:11:08 -0700

    Save data to dictionary instead of printing it

--- M_fasta_sequence_length.py
+++ M_fasta_sequence_length.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 from Bio import SeqIO
 import sys
+import pprint
 
 # Modified from http://bioexpressblog.wordpress.com/2014/04/15/calculate-length-of-all-sequences-in-an-multi-fasta-file/
 
+fasta_sizes = dict()
 cmdargs = str(sys.argv)
 for seq_record in SeqIO.parse(str(sys.argv[1]), "fasta"):
-	output_line = "{0}\t{1}".format(seq_record.id, len(seq_record))
-	print(output_line)
+	fasta_sizes[seq_record.id] = len(seq_record)
+
+print fasta_sizes['EUKY_12|gi|134108779|ref|NC_009178.1|_Cryptococcus_neoformans_var._neoformans_B-3501A_chromosome_2,_whole_genome_shotgun_sequence913558']
