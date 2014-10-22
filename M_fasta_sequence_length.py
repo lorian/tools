d3e80c4308dfccf74b5e06b3ebab2abe54906965
Lorian <lanthala@gmail.com>
2014-10-22 11:00:10 -0700

    Prints unusually short fasta lengths

--- M_fasta_sequence_length.py
+++ M_fasta_sequence_length.py
@@ -6,12 +6,16 @@ import csv
 
 # Modified from http://bioexpressblog.wordpress.com/2014/04/15/calculate-length-of-all-sequences-in-an-multi-fasta-file/
 
+print "Getting lengths from fasta..."
 # Get fasta lengths
 fasta_sizes = dict()
 cmdargs = str(sys.argv)
 for seq_record in SeqIO.parse(str(sys.argv[1]), "fasta"):
 	fasta_sizes[seq_record.id] = len(seq_record)
+	if len(seq_record) < 1000:
+		print "Unusually short record {0} bases: \n\t{1}".format(len(seq_record),seq_record.id)
 
+print "Comparing lengths with sam..."
 # Format of sam header: @SQ, SN:<name>, LN:<length>
 sam_header = csv.reader(open(sys.argv[2],'r'),delimiter='\t')
 for row in sam_header:
