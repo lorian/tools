f73230b5475b5f89cd5c2f2d6fbb93cf70acc483
Lorian <lanthala@gmail.com>
2015-05-18 12:14:51 -0700

    Convert mfa files downloaded for kallisto into CLARK files

---
+++ M_prep_clark_files.py
@@ -0,0 +1,44 @@
+'''
+Copy multifastas into directory structure needed for CLARK
+'''
+
+import os
+import sys
+import lanthpy
+
+def mkdir(dirname):
+	''' Make directory for species in clark folder '''
+	clark = os.path.expanduser(os.path.join('~/metagenome/clark/',
+				lanthpy.genome_name_cleanup([dirname.partition(".")[0]])[0]))
+
+	if not os.path.exists(clark):
+		os.makedirs(clark)
+
+	return clark
+
+def mkfile(filename,dirname):
+	single_fasta = open(os.path.join(dirname,
+			lanthpy.genome_name_cleanup([filename.rstrip().split('|')[-1]])[0] +".fna"), 'wt')
+	return single_fasta
+
+
+def main():
+	dirname = ' '.join(sys.argv[1:])
+	dirname.lstrip(" ")
+
+	file_list = os.listdir(dirname)
+	for f in file_list:
+		if f.endswith('fa') or f.endswith('fasta'):
+			fdir = mkdir(f)
+
+			mfa = open(os.path.join(dirname,f),'r')
+			for line in mfa:
+				if line[0] == '>': # fasta name
+					ffile = mkfile(line,fdir)
+					ffile.write(line)
+				else:
+					ffile.write(line)
+
+
+if __name__ == '__main__':
+	main()
