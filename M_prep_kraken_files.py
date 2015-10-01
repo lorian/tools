b900c45e58543ced83e9560ed3ab44dd402e9e4d
Lorian <lanthala@gmail.com>
2015-09-30 21:03:44 -0700

    Parameterized file name

--- M_prep_kraken_files.py
+++ M_prep_kraken_files.py
@@ -1,7 +1,14 @@
-import os
+# Remove text in front of gi| in fasta header so kraken and clark can parse them.
 
-mfa = open('i100.fasta','r')
-new_mfa = open('i100_kraken.fasta','w')
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('fasta_filename')
+args = parser.parse_args()
+
+mfa = open(args.fasta_filename,'r')
+new_mfa = open(args.fasta_filename.rpartition('.')[0] + '_kraken.fasta','w')
 for line in mfa:
 	if line[0] == '>': # fasta name
 		new_mfa.write('>gi|' + line.partition('gi|')[2]) # remove the strain label at the front of the header because kraken is stupid
