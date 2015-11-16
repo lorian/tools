f10cee65335bce2885bfdd5207be7152face74de
Lorian <lanthala@gmail.com>
2015-11-16 14:14:43 -0800

    Added echo of each command

--- M_pipeline_clark.sh
+++ M_pipeline_clark.sh
@@ -5,8 +5,13 @@ mkdir $DIR
 cd $DIR
 mkdir Custom
 cd Custom \
+&& echo python ~/tools/M_prep_clark_files_single.py ~/scratch/${FASTA}.mfa \
 && python ~/tools/M_prep_clark_files_single.py ~/scratch/${FASTA}.mfa \
 && cd ~/scratch/clark \
+&& echo ./set_targets.sh $DIR custom \
 && time ./set_targets.sh $DIR custom \
+&& echo ./classify_metagenome.sh -P ~/scratch/kallisto_data/illumina_100species_trimmed.1.fq ~/scratch/kallisto_data/illumina_100species_trimmed.2.fq -R clark_${FASTA} \
 && time ./classify_metagenome.sh -P ~/scratch/kallisto_data/illumina_100species_trimmed.1.fq ~/scratch/kallisto_data/illumina_100species_trimmed.2.fq -R clark_${FASTA} \
+&& echo ./estimate_abundance.sh -F clark_${FASTA}.csv -D $DIR > ${FASTA}.clark \
 && time ./estimate_abundance.sh -F clark_${FASTA}.csv -D $DIR > ${FASTA}.clark \
+&& echo Output in ${FASTA}.clark
--- M_pipeline_kallisto.sh
+++ M_pipeline_kallisto.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-FASTA=$1 #accepts argument of fasta name, WITHOUT file extension (which needs to be .mfa)
-time /usr/local/bin/kallisto_fast_loose index -i {$FASTA}_index {$FASTA}.mfa \
-&& time /usr/local/bin/kallisto_fast_loose quant --fast-and-loose -i {$FASTA}_index -o $FASTA illumina_100species_trimmed.1.fq illumina_100species_trimmed.2.fq \
+FASTA=$1 #accepts argument of fasta name, WITHOUT path info or file extension (which needs to be .mfa)
+echo kallisto index -i ${FASTA}_index ~/scratch/${FASTA}.mfa \
+&& time kallisto index -i ${FASTA}_index ~/scratch/${FASTA}.mfa \
+&& echo kallisto quant -i ${FASTA}_index -o $FASTA illumina_100species_trimmed.1.fq illumina_100species_trimmed.2.fq \
+&& time kallisto quant -i ${FASTA}_index -o $FASTA illumina_100species_trimmed.1.fq illumina_100species_trimmed.2.fq \
 && cd $FASTA \
-&& mv abundance.tsv ../{$FASTA}_abundance.txt
+&& mv abundance.tsv ../${FASTA}_abundance.txt \
+&& cd ../ \
+&& echo Output in ${FASTA}_abundance.txt
+
