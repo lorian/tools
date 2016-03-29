de006b31cce996bd2dae1fa789c473dc2e9b6b04
Lorian <lanthala@gmail.com>
2016-03-28 19:34:56 -0700

    Creates t2g file correctly where individual fastas are the "transcripts" and strain names are the "genes"

--- M_create_t2g_sleuth_file.py
+++ M_create_t2g_sleuth_file.py
@@ -16,33 +16,40 @@ parser.add_argument('filename', help='Output file of kallisto')
 args = parser.parse_args()
 filename = args.filename
 
+'''
 global tax_dict
 pickle_len = 0
 if os.path.exists('species_taxonomy.pickle'):
 	print "Loading taxonomy dict..."
 	tax_dict = cPickle.load(open('species_taxonomy.pickle','rb'))
 	pickle_len = len(tax_dict.names.keys())
-
+'''
 raw_est = Dataset()
 input_csv = csv.reader(open(filename,'r'), 'excel-tab')
 input_data = [r for r in input_csv]
 input_data = input_data[1:] #remove header row
 raw_est.species = zip(*input_data)[0]
+raw_est.counts = raw_est.null_list()
+raw_est.abundance = raw_est.null_list()
 
 print "Number of raw entries: {0}".format(len(raw_est.species))
 
-raw_est.clean_names()
 raw_est.remove_matches('rna') # remove specific genes
 raw_est.remove_matches('gene_')
+raw_est.remove_matches('plasmid')
 
-print raw_est.species
+with open("t2g_strains.txt","w") as f:
+	f.write("target_id\tstrain\n")
+	for st in raw_est.species:
+		f.write("{}\t{}\n".format(st,st.partition('|')[0].partition('_gi')[0].replace('_',' ')))
 
+raw_est.clean_names()
 est = collapse_duplicates(raw_est)
+
+'''
 est.species = lookup_tax_list(est.species) # from this point on, species are taxids
-
-
-
 
 if pickle_len != len(tax_dict.names.keys()): # don't re-pickle if nothing new is added
 	print "Saving taxonomy dict..."
 	cPickle.dump(tax_dict,open('species_taxonomy.pickle','wb'))
+'''
