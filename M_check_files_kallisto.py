8ce861e0440112b60edc70a4abe97b6d1edf546f
Lorian <lanthala@gmail.com>
2016-04-19 16:37:58 -0700

    Same thing but in reverse

--- M_check_files_kallisto.py
+++ M_check_files_kallisto.py
@@ -8,7 +8,8 @@ def split_files(f):
 	split_B = f[len(f)/2:]
 	return split_A,split_B
 
-bad_files = [f for f in os.listdir('.') if f.endswith('.cdna.all.fa')].reverse()
+bad_files = [f for f in os.listdir('.') if f.endswith('.cdna.all.fa')]
+bad_files.reverse()
 
 for f in bad_files:
 	if test_kallisto([f]):
