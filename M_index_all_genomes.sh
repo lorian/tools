7d3fd583e7d0197a28876f84550e048abea08000
Lorian <lanthala@gmail.com>
2016-05-05 13:11:37 -0700

    Log all output to files

--- M_index_all_genomes.sh
+++ M_index_all_genomes.sh
@@ -4,7 +4,7 @@ for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
 	cd part_${INDEX}/
-	time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa \
-	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o kallisto_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 \
+	time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa > log_index_${INDEX}.txt 2>&1 \
+	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o kallisto_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 > log_align_${INDEX}.txt 2>&1 \
 	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt
 done
