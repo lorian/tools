53a5f3a34e854aad934f082f50149e96b37ca1a2
Lorian <lanthala@gmail.com>
2016-05-05 13:08:35 -0700

    Fixed script errors

--- M_index_all_genomes.sh
+++ M_index_all_genomes.sh
@@ -4,7 +4,7 @@ for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
 	cd part_${INDEX}/
-	&& time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa \
+	time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa \
 	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o kallisto_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 \
-	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt \
+	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt
 done
