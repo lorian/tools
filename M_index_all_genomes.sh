f8d68cb021d3a1dc93e18611381c61b585b2f138
Lorian <lanthala@gmail.com>
2016-05-05 12:53:27 -0700

    Index and pseudoalign to representative strains broken into chunks

---
+++ M_index_all_genomes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+indexes=(2 3 4 5)
+for INDEX in "${indexes[@]}"
+do
+	echo -e "\n\e[7m$INDEX\e[0m"
+	cd part_${INDEX}/
+	&& time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa \
+	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o kallisto_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 \
+	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt \
+done
