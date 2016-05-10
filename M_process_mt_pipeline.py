5ac259026e40278503d1d64ebc886e5e00eae271
Lorian <lanthala@gmail.com>
2016-05-09 20:22:53 -0700

    Lots of handling of inappropriate characters in filenames

--- M_process_mt_pipeline.py
+++ M_process_mt_pipeline.py
@@ -13,9 +13,12 @@ for sp in hits:
 	name = sp.lower().replace(" ","_")
 	output = os.system("ls {}*".format(name))
 	if output != 0: # ls can't match
-		name = name.replace('.',"").replace("-","_")
+		name = name.replace('.',"").replace("-","_").replace("(","_").replace(")","_").replace("+","_").replace('[',"").replace(']',"").replace("/","_")
 		output = os.system("ls {}*".format(name))
 		if output != 0:
+			name = sp.partition('_')[0] # get whole genus
+			output = os.system("ls {}*".format(name))
 			failed_hits.append(sp)
 
 pprint.pprint(failed_hits)
+print len(failed_hits)
