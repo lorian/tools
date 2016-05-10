566ea7d92353b9d2b53c75caa41c05c806deae0a
Lorian <lanthala@gmail.com>
2016-05-09 19:58:36 -0700

    Handle species that don't match filenames

--- M_process_mt_pipeline.py
+++ M_process_mt_pipeline.py
@@ -2,12 +2,20 @@
 
 import csv
 import os
+import pprint
 
 with open('species_hits.txt','r') as hits_file:
 	hits_csv = csv.reader(hits_file, delimiter=',')
-	hits = [r[0] for r in hits_csv if float(r[1])>50]
-	print hits
-	print len(hits)
+	hits = [r[0] for r in hits_csv if float(r[1])>100]
 
+failed_hits = []
 for sp in hits:
-	os.system("ls {}*".format(sp.lower().replace(" ","_")))
+	name = sp.lower().replace(" ","_")
+	output = os.system("ls {}*".format(name))
+	if output != 0: # ls can't match
+		name = name.replace('.',"").replace("-","_")
+		output = os.system("ls {}*".format(name))
+		if output != 0:
+			failed_hits.append(sp)
+
+pprint.pprint(failed_hits)
