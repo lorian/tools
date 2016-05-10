24329f37a474ee43eda51afd61db32a7c59ac315
Lorian <lanthala@gmail.com>
2016-05-10 10:17:12 -0700

    Switch ls to mv

--- M_process_mt_pipeline.py
+++ M_process_mt_pipeline.py
@@ -6,19 +6,20 @@ import pprint
 
 with open('species_hits.txt','r') as hits_file:
 	hits_csv = csv.reader(hits_file, delimiter=',')
-	hits = [r[0] for r in hits_csv if float(r[1])>100]
+	hits = [r[0] for r in hits_csv if float(r[1])>200]
 
 failed_hits = []
 for sp in hits:
 	name = sp.lower().replace(" ","_")
-	output = os.system("ls {}*".format(name))
-	if output != 0: # ls can't match
+	output = os.system("mv {}* ../".format(name))
+	if output != 0: # mv can't match
 		name = name.replace('.',"").replace("-","_").replace("(","_").replace(")","_").replace("+","_").replace('[',"").replace(']',"").replace("/","_")
-		output = os.system("ls {}*".format(name))
+		output = os.system("mv {}* ../".format(name))
 		if output != 0:
 			name = sp.partition('_')[0] # get whole genus
-			output = os.system("ls {}*".format(name))
+			output = os.system("mv {}* ../".format(name))
 			failed_hits.append(sp)
 
 pprint.pprint(failed_hits)
 print len(failed_hits)
+print len(hits)
