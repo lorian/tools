17710c8618476d00491e6c057d45482d75ea9dc4
Lorian <lanthala@gmail.com>
2016-05-10 09:48:06 -0700

    Align to 7 genome indexes

---
+++ M_align_all_genomes.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+indexes=(1 2 3 4 6 7)
+FASTQ="SRR769395"
+for INDEX in "${indexes[@]}"
+do
+	echo -e "\n\e[7m$INDEX\e[0m"
+	echo ${FASTQ}
+	time kallisto quant -i singlesp_genome_${INDEX}_index -o real_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTA}_1.fastq.gz ~/scratch/mt_real/${FASTA}_2.fastq.gz -t 30 > log_align_${FASTQ}_${INDEX}.txt 2>&1 \
+	&& mv real_singlesp_genome_${INDEX}/abundance.tsv ../real_singlesp_genome_${FASTQ}_${INDEX}_abundance.txt
+done
