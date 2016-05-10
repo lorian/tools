6ab9b22ccd1f8e8eb73c5ce45b9164f23a0b6652
Lorian <lanthala@gmail.com>
2016-05-10 09:47:26 -0700

    Index 7 genome indexes

--- M_index_all_genomes.sh
+++ M_index_all_genomes.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-indexes=(1 2 3 4 5)
-FASTQ="SRR769395"
+indexes=(1 2 3 4 5 6 7)
 for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
-	echo ${FASTQ}
-	time kallisto quant -i singlesp_genome_${INDEX}_index -o real_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTA}_1.fastq.gz ~/scratch/mt_real/${FASTA}_2.fastq.gz -t 30 > log_align_${FASTQ}_${INDEX}.txt 2>&1 \
-	&& mv real_singlesp_genome_${INDEX}/abundance.tsv ../real_singlesp_genome_${FASTQ}_${INDEX}_abundance.txt
+	cd ../part_${INDEX}/
+	time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa > log_index_${INDEX}.txt 2>&1 \
+	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o kallisto_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 > log_align_${INDEX}.txt 2>&1 \
+	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt
 done
