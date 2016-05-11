0b6d04ab87d0bce968cb99ef80fc8e585500bd9a
Lorian <lanthala@gmail.com>
2016-05-11 09:15:53 -0700

    Fix directories

--- M_temp_index.sh
+++ M_temp_index.sh
@@ -5,6 +5,7 @@ FASTQ="ld_7point5mill_sample_processed.fasta"
 for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
+	cd ~/scratch/ensembl_bact/
 	time kallisto quant -i singlesp_genome_${INDEX}_index -o sim_singlesp_genome_${INDEX} --single ~/scratch/mt_sim/ld_7point5mill_sample_processed.fasta -l 200 -s 5 -t 30 > log_singlesp_genome_sim_${INDEX}.txt 2>&1 \
 	&& mv sim_singlesp_genome_${INDEX}/abundance.tsv ../sim_singlesp_genome_${INDEX}_abundance.txt
 done
@@ -14,7 +15,7 @@ FASTQ="SRR769395"
 for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
-	cd ~/scratch/ensemble_bact/part_${INDEX}/
+	cd ~/scratch/ensembl_bact/part_${INDEX}/
 	time kallisto index -i singlesp_genome_${INDEX}_index *.dna.genome.fa > log_index_${INDEX}.txt 2>&1 \
 	&& time kallisto quant -i singlesp_genome_${INDEX}_index -o real_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTA}_1.fastq.gz ~/scratch/mt_real/${FASTA}_2.fastq.gz -t 30 > log_align_${FASTQ}_${INDEX}.txt 2>&1 \
 	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt
