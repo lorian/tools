e99827fd9c90e28ab836adc74fb808da62af77b0
Lorian <lanthala@gmail.com>
2016-05-12 19:15:16 -0700

    Run through real transcriptome indexes

--- M_temp_index.sh
+++ M_temp_index.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-indexes=("2_1" "2_2" "2_3" "2_4" "2_5" "2_6")
+indexes=("entero" "klebsiella" "shigella" "misc" "ecoli_1" "ecoli_2" "ecoli_3" "ecoli_4")
 FASTQ="SRR769395"
 for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
-	cd ~/scratch/ensembl_bact/part_${INDEX}/
-	time kallisto quant -i singlesp_genome_${INDEX}_index -o real_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTQ}_1.fastq.gz ~/scratch/mt_real/${FASTQ}_2.fastq.gz -t 30 > log_align_${FASTQ}_${INDEX}.txt 2>&1 \
-	&& mv kallisto_singlesp_genome_${INDEX}/abundance.tsv ../kallisto_singlesp_genome_${INDEX}_abundance.txt
+	cd ~/scratch/ensembl_cdna/real_${INDEX}/
+	time kallisto index -i real_${INDEX}_index *.fa > ../log_index_${INDEX}.txt 2>&1 \
+	&& time kallisto quant -i real_${INDEX}_index -o real_${INDEX} ~/scratch/mt_real/${FASTQ}_1.fastq.gz ~/scratch/mt_real/${FASTQ}_2.fastq.gz -t 30 > ../log_align_${FASTQ}_${INDEX}.txt 2>&1 \
+	&& mv real_${INDEX}/abundance.tsv ../real_${INDEX}_abundance.txt
 done
