a8979990bb6186e00308b20fc59a0ca9ca43f822
Lorian <lanthala@gmail.com>
2016-05-13 00:24:50 -0700

    Set threshold back to 1000

--- M_process_mt_pipeline.py
+++ M_process_mt_pipeline.py
@@ -7,7 +7,7 @@ import string
 
 with open('species_hits.txt','r') as hits_file:
 	hits_csv = csv.reader(hits_file, delimiter=',')
-	hits = [r[0] for r in hits_csv if float(r[1])>10000]
+	hits = [r[0] for r in hits_csv if float(r[1])>1000]
 
 failed_hits = []
 for sp in hits:
