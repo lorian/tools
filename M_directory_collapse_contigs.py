0ba8b2503fb5e774ececf02ba0ff0d04e9a4f0fc
Lorian <lanthala@gmail.com>
2016-07-01 14:04:37 -0700

    Collapse contigs of dna.genome.fa files into single fasta separated by Ns

---
+++ M_directory_collapse_contigs.py
@@ -0,0 +1,39 @@
+# Concatenate the entries of a multi-entry fasta file into one single entry, for all mfas in a directory
+
+import sys
+import os
+
+dirname = ""
+iterarg = iter(sys.argv)
+next(iterarg) #skip name of function
+for arg in iterarg:
+	if dirname == "":
+		dirname = arg #avoid extra space at beginning
+	else:
+		dirname = dirname + " " + arg
+
+file_list = [f for f in os.listdir(dirname) if f.endswith('dna.genome.fa')]
+
+for f in file_list:
+	basename = f.lpartition('.')[0]
+	mfa = open(os.path.join(dirname,f),'r')
+
+	text = ""
+
+	firstline = True
+	for line in mfa:
+		if firstline:
+#			text = '>' + basename.replace (" ", "_") + " " + line[1:] #add name to beginning of ID line
+			text = line
+			firstline = False
+		elif line.startswith('>'):
+			text += 'NNNNNNNNNN' #indicate possible gaps between chr, plasmids, shotgun pieces, etc
+		elif line!= "\n": #skip empty lines within fasta
+			text += line
+
+	text = text + "\n" #add newline to end of file for eventual cat
+	fa = open(os.path.join(dirname,basename + '.cat.fa'), 'w')
+	fa.seek(0)
+	fa.write(text)
+	fa.truncate()
+	fa.close()
