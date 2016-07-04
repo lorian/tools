66e78de203495a6c274b394a14270740ad8b7aed
Lorian <lanthala@gmail.com>
2016-07-03 17:46:31 -0700

    Replace header with name only

--- M_directory_collapse_contigs.py
+++ M_directory_collapse_contigs.py
@@ -25,8 +25,6 @@ for f in file_list:
 		if firstline:
 			# replace header with name
 				text = '>' + basename.partition('.')[0].replace(" ", "_") + "|"
-			else:
-				text = line
 			firstline = False
 		elif line.startswith('>'):
 			text += 'NNNNNNNNNN' #indicate possible gaps between chr, plasmids, shotgun pieces, etc
