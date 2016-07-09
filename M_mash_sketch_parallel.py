39dcc33eeb0e2ca09cbd6db88a35e431e8455c4f
Lorian <lanthala@gmail.com>
2016-07-09 16:47:59 -0700

    Time is std output not error

--- M_mash_sketch_parallel.py
+++ M_mash_sketch_parallel.py
@@ -5,4 +5,4 @@ import string
 
 for letter in string.ascii_uppercase+"_":
 	print letter
-	subprocess.call("time mash sketch -o ensembl_bact_{0} {0}*.dna.genome.fa -s 10000 -k 25 2> log_bact_{0} &".format(letter), shell=True)
+	subprocess.call("time mash sketch -o ensembl_bact_{0} {0}*.dna.genome.fa -s 10000 -k 25 &> log_bact_{0} &".format(letter), shell=True)
