ecfd70384380c76dceee9c4b22f2ce9afe6b205e
Lorian <lanthala@gmail.com>
2016-07-11 17:04:16 -0700

    Report time of each run

--- M_mash_dist_parallel.py
+++ M_mash_dist_parallel.py
@@ -5,4 +5,4 @@ import string
 
 for letter in string.ascii_uppercase+"_":
 	print letter
-	subprocess.call('mash dist ensembl_bact_{0}.msh ~/scratch/illumina_100species_trimmed.all.fq > mash_i100_4_{0}.txt &'.format(letter), shell=True)
+	subprocess.call('time mash dist ensembl_bact_{0}.msh ~/scratch/illumina_100species_trimmed.all.fq > mash_i100_3_{0}.txt &'.format(letter), shell=True)
