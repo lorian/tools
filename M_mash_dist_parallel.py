e7edc89edcbc6d89dedd9fa37d4f0c72559924fa
Lorian <lanthala@gmail.com>
2016-07-12 13:49:38 -0700

    Takes fastq file as input; runs in any directory; cat's all resulting files.

--- M_mash_dist_parallel.py
+++ M_mash_dist_parallel.py
@@ -2,7 +2,14 @@
 
 import subprocess
 import string
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('fastq', help='Fastq file to calculate distance from mash indexes')
+args = parser.parse_args()
 
 for letter in string.ascii_uppercase+"_":
 	print letter
-	subprocess.call('time mash dist ensembl_bact_{0}.msh ~/scratch/illumina_100species_trimmed.all.fq > mash_i100_3_{0}.txt &'.format(letter), shell=True)
+	subprocess.call('time mash dist ~/scratch/ensembl_bact/ignore/ensembl_bact_{0}.msh {2} > mash_real_{1}_{0}.txt &'.format(letter, args.fastq.partition('.')[0], args.fastq), shell=True)
+
+subprocess.call('cat mash_real_{0}_* > mash_real_{0}.txt'.format(args.fastq.partition('.')[0]), shell=True)
