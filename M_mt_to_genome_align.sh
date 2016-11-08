2e39b390e9b70bf87105c531137db8337e065d4c
Lorian <lanthala@gmail.com>
2016-11-07 22:27:32 -0800

    Fixed variable

--- M_mt_to_genome_align.sh
+++ M_mt_to_genome_align.sh
@@ -5,6 +5,6 @@ for INDEX in "${indexes[@]}"
 do
 	echo -e "\n\e[7m$INDEX\e[0m"
 	echo ${FASTQ}
-	time kallisto quant -i singlesp_genome_${INDEX}_index -o real_DNA_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTA}_1.fastq ~/scratch/mt_real/${FASTA}_2.fastq -t 30 \
+	time kallisto quant -i singlesp_genome_${INDEX}_index -o real_DNA_singlesp_genome_${INDEX} ~/scratch/mt_real/${FASTQ}_1.fastq ~/scratch/mt_real/${FASTQ}_2.fastq -t 30 \
 	&& mv real_DNA_singlesp_genome_${INDEX}/abundance.tsv ../real_DNA_singlesp_genome_${FASTQ}_${INDEX}_abundance.txt
 done
