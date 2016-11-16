02e969232597f4f23791c904a4fea9e2019910af
Lorian <lanthala@gmail.com>
2016-11-15 18:02:51 -0800

    Fixed error handling when taxid fails

--- MT_preprocess_coman.py
+++ MT_preprocess_coman.py
@@ -1,15 +1,17 @@
 # Preprocess transcript file to add strain names
 import argparse
 import csv
+from Bio import Entrez
+Entrez.email = 'lanthala@berkeley.edu'
 
 
-mfa = open('processed'+args.filename,'w')
+mfa = open('processed_ReadCount_dataset.txt','w')
 
 with open('gene_ReadCount_dataset.txt','r') as transcripts:
 	for line in transcripts:
 		if line.startswith('gi|'):
 			# look up gi number: gi|479337003|ref|YP_007848221.1|
-			gi = line.partition('gi|')[2].partition('|')[0]
+			uid = line.partition('gi|')[2].partition('|')[0]
 
 			handle = Entrez.efetch("nucleotide", id=uid, retmode="xml")
 			records = Entrez.read(handle)
@@ -18,8 +20,12 @@ with open('gene_ReadCount_dataset.txt','r') as transcripts:
 			if not taxid:
 				# sometimes it's not the last quals list, so we have to search for it
 				taxid = [r['GBQualifier_value'] for r in records[0]['GBSeq_feature-table'][0]['GBFeature_quals'] if ('taxon' in r['GBQualifier_value'])][0].partition(':')[2]
-
-			mfa.write('taxid|'+ taxid +'|'+ line)
+			
+			if taxid.isdigit():
+				mfa.write('taxid|'+ taxid +'|'+ line)
+			else:
+				print "Error with {}".format(line)
+				mfa.write(line)
 		else:
 			mfa.write(line)
 
