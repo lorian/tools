9b7810585eb15c9a7d0df939af3eaadecda2c465
Lorian <lanthala@gmail.com>
2016-11-17 18:23:10 -0800

    Convert back to running on single ortho file

--- MT_lookup_orthology.py
+++ MT_lookup_orthology.py
@@ -28,16 +28,12 @@ else:
 			pathway_dict[line[1]].add(pathway)
 	
 	# get dict of gene ids to KO ids
-	ortho_files = [f for f in os.listdir('.') if f.startswith("meta.ortho_")]
 	annotation_dict = dict()
-	for f in ortho_files:
-		with open(f,'r') as anno_file:
-			anno_csv = csv.reader(anno_file, delimiter=' ', skipinitialspace=True)
-			anno_data = [r for r in anno_csv]
-			anno_dict = {line[1]:pathway_dict[line[5]] for line in anno_data if line[5] in pathway_dict.keys()} # gene id = pathway category
-			cPickle.dump(anno_dict,open(f+'.pickle','wb'))
-		annotation_dict.update(anno_dict)
-		print "Processed {}".format(f)
+	with open('meta.ortho2','r') as anno_file:
+		anno_csv = csv.reader(anno_file, delimiter=' ', skipinitialspace=True)
+		anno_data = [r for r in anno_csv]
+		annotation_dict = {line[1]:pathway_dict[line[5]] for line in anno_data if line[5] in pathway_dict.keys()} # gene id: set(pathway category)
+
 	print "Saving annotation dict..."
 	cPickle.dump(annotation_dict,open('gene_annotation.pickle','wb'))
 
